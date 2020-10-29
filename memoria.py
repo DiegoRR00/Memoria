b9a30002c528297bee59efcc48bae27718814e60
Marco Bosquez <60762402+MarcoBosglez@users.noreply.github.com>
2020-10-29 11:26:00 -0600

    Update digitos alineados y contador de taps
    Update: Contar y desplegar el numero de taps en la parte superior derecha
    
    Update: Central el dígito en el cuadro

--- memoria.py
+++ memoria.py
@@ -1,12 +1,13 @@
 from random import *
 from turtle import *
 from freegames import path
-
+#Variables inciales
+num = {'taps': 0}
 car = path('car.gif')
-tiles = list(range(32)) * 2
+tiles = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","#","$","%","&","+","-"]*2
 state = {'mark': None}
 hide = [True] * 64
-
+#Dibujar el cuadrado en coordenadas
 def square(x, y):
     "Draw white square with black outline at (x, y)."
     up()
@@ -18,56 +19,67 @@ def square(x, y):
         forward(50)
         left(90)
     end_fill()
-
+#Encontrar el índice de una coordenada
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
-
+#Convierte un tila a coordenadas
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
-
+#Registra entrada en el tablero
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
+    writer.undo()
+    writer.write(num['taps'])
+    
     spot = index(x, y)
     mark = state['mark']
 
-    if mark is None or mark == spot or tiles[mark] != tiles[spot]:
+    if mark is None or mark == spot or tiles[mark] != tiles[spot]:#Primera selección
+        num['taps'] += 1
         state['mark'] = spot
-    else:
+    else:#Segunda selección
+        num['taps'] += 1
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
 
+#Imprime todo y evalúa
 def draw():
     "Draw image and tiles."
     clear()
     goto(0, 0)
     shape(car)
     stamp()
-
+    #Imprime los tableros necesarios
     for count in range(64):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
 
     mark = state['mark']
-
+    #Imprime el valor
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 26, y)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
-
+        write(tiles[mark], align = 'center', font = ('Arial', 30, 'normal'))
+    if True not in hide:#Si no queda nada oculto
+        print("Se acabo")#Impriir en consola
+        done()#Finalizar el ontimer
+        return
     update()
     ontimer(draw, 100)
-
-shuffle(tiles)
+shuffle(tiles)#Revolver fichas
 setup(420, 420, 370, 0)
-addshape(car)
+addshape(car)#Poner el carro en el fondo
 hideturtle()
 tracer(False)
 onscreenclick(tap)
 draw()
+writer.goto(210, 210)
+writer.color('black')
+writer.write(num['taps'])
 done()
